ef99c041eadfac1fc9fbd3a24906ed0fa4f0444d
Suraj <surajgautam1111@gmai;l.com>
2023-11-04 18:44:19 +0545

    beginning

--- NodeJs Completed Guide/app.js
+++ NodeJs Completed Guide/app.js
@@ -1,11 +1,23 @@
-const http = require('http');
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-const routes = require('./routes');
+app.set('view engine' , 'pug')
+app.set('views','views')
 
-console.log(routes.someText);
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const server = http.createServer(routes.handler);
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/admin',adminData.routes)
+app.use(shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})
+
+app.listen(3000);
 
 
---
+++ NodeJs Completed Guide/routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+const rootDir = require('../util/path')
+const router = express.Router();
+const adminData = require('./admin')
+router.get('/', (req, res, next) => {
+  console.log('shop.js',adminData.products)
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
